cd9f98aeb101857d8215a7c21ed446f5aeaecc3c
armen <arknyazyan@gmail.com>
2019-06-18 19:52:51 +0400

    page scroll

--- src/components/cCursor.vue
+++ src/components/cCursor.vue
@@ -17,20 +17,16 @@
             cursor.style.height = 10 + 'px';
             cursor.style.width = 10 + 'px';
             document.addEventListener( 'mousemove', (e) => {
-                cursor.style.top = e.pageY - (parseInt(cursor.style.height) / 2) + 'px'
-                cursor.style.left = e.pageX - (parseInt(cursor.style.width) / 2) + 'px'
+                cursor.style.top = e.clientY - (parseInt(cursor.style.height) / 2) + 'px'
+                cursor.style.left = e.clientX - (parseInt(cursor.style.width) / 2) + 'px'
             })
             let pointer = document.getElementsByClassName('cursor-pointer');
             for (let i = 0; i < pointer.length; i++) {
                 pointer[i].addEventListener( 'mouseover', () => {
                     this.cursor_pointer = true
-                    cursor.style.transition = 0.08 + 's'
                 })
                 pointer[i].addEventListener( 'mouseleave', () => {
                     this.cursor_pointer = false
-                    setTimeout( () => {
-                        cursor.style.transition = 0 + 's'
-                    },200)
                 })
             }
         }
--- src/layout/header.vue
+++ src/layout/header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['header', {'z-index' : header_z}]">
+  <div :class="['header', {'z-index' : header_z, 'opacity': header_opacity == '1'}, {'header-hide': header_hide}]">
     <div class="language">
       <span @click="setLang('en')">ru/eng</span>
     </div>
@@ -9,8 +9,14 @@
               src="../assets/images/user.png"
       >
     </div>
-    <div class="menu">
-      <span>{{$t('message')}}</span>
+    <div @click="menu_show = !menu_show" class="menu cursor-pointer">
+      <div class="menu-icon">
+        <span :class="['menu-line menu-line1', {'menu-show': menu_show}]"></span>
+        <span :class="['menu-line menu-line2', {'menu-show': menu_show}]"></span>
+        <span :class="['menu-line menu-line3', {'menu-show': menu_show}]"></span>
+      </div>
+      <span class="menu-text">{{menu_show ? 'Close' : 'Menu'}}</span>
+<!--      <span>{{$t('message')}}</span>-->
     </div>
   </div>
 </template>
@@ -22,13 +28,35 @@
       return {
         user_photo_opacity: 0,
         header_z: false,
+        menu_show: false,
+        header_hide: true,
+        header_opacity: 1,
       }
     },
     mounted(){
       setTimeout( () => {
+        this.header_hide = false
+      },11400)
+      setTimeout( () => {
         this.user_photo_opacity = 1;
         this.header_z = true;
+        this.header_hide = false
       },12700)
+      let windowScrollTop = [window.scrollY];
+      document.addEventListener( 'scroll', (e) => {
+        windowScrollTop.push(window.scrollY)
+        if (windowScrollTop[windowScrollTop.length - 2] - windowScrollTop[windowScrollTop.length - 1] > 0) {
+          //scroll top
+          this.header_hide = false
+        } else {
+          //scroll bottom
+          this.header_hide = true
+          this.header_opacity = ''
+        }
+        if (window.scrollY == 0) {
+          this.header_opacity = 1
+        }
+      })
     },
     methods: {
       setLang: function(lang){
@@ -48,8 +76,9 @@
     padding: 3px 40px;
     background: #ffffff;
     border-bottom: 1px solid #ddd8;
-    transform: translateY(-100%);
-    opacity: 0;
+    transition: .4s all;
+    transform: translateY(0);
+    opacity: 0.7;
     animation: headerShow;
     animation-delay: 11s;
     animation-duration: 0.9s;
@@ -57,6 +86,13 @@
     animation-timing-function: linear;
     &.z-index{
       z-index: 1;
+    }
+    &.header-hide{
+      transform: translateY(-100%);
+      opacity: 0;
+    }
+    &.opacity{
+      opacity: 1;
     }
     .user-photo{
       height: 60px;
@@ -70,13 +106,52 @@
     }
     .menu{
       justify-self: end;
+      display: flex;
+      align-items: center;
+
+      .menu-text{
+        margin-left: 4px;
+      }
+      .menu-icon{
+        position: relative;
+        width: 18px;
+        height: 12px;
+
+        .menu-line{
+          position: absolute;
+          width: 100%;
+          height: 2px;
+          background-color: #000000;
+          display: inline-block;
+          transition: .2s;
+          transform-origin: 50% 50%;
+        }
+        .menu-line1{
+          top: 0;
+          &.menu-show{
+            transform: rotate(-45deg) translateX(-3px) translateY(4px);
+          }
+        }
+        .menu-line2{
+          top: 5px;
+          &.menu-show{
+            width: 0;
+          }
+        }
+        .menu-line3{
+          top: 10px;
+          &.menu-show{
+            transform: rotate(45deg) translateX(-3px) translateY(-4px);
+          }
+        }
+      }
     }
   }
 
-  @keyframes headerShow {
-    100%{
-      transform: translateY(0);
-      opacity: 1;
-    }
-  }
+  /*@keyframes headerShow {*/
+  /*  100%{*/
+  /*    transform: translateY(0);*/
+  /*    opacity: 1;*/
+  /*  }*/
+  /*}*/
 </style>
