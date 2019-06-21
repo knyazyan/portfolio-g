a3040cced8a9faf584c79970732c3dd8ea159ab9
armen1995 <arknyazyan@gmail.com>
2019-06-21 10:21:01 +0400

    fix

--- src/layout/header.vue
+++ src/layout/header.vue
@@ -1,21 +1,31 @@
 <template>
   <div :class="['header', {'z-index' : header_z, 'opacity': header_opacity == '1'}, {'header-hide': header_hide}]">
-    <div class="language">
-      <span @click="setLang('en')">ru/eng</span>
-    </div>
-    <div class="user-photo cursor-pointer">
-      <img
-              :style="{'opacity': user_photo_opacity}"
-              src="../assets/images/user.png"
-      >
-    </div>
-    <div @click="menu_show = !menu_show" class="menu cursor-pointer">
-      <div class="menu-icon">
-        <span :class="['menu-line menu-line1', {'menu-show': menu_show}]"></span>
-        <span :class="['menu-line menu-line2', {'menu-show': menu_show}]"></span>
-        <span :class="['menu-line menu-line3', {'menu-show': menu_show}]"></span>
+    <div class="left-head">
+      <div class="user-photo cursor-pointer">
+        <img
+          :style="{'opacity': user_photo_opacity}"
+          src="../assets/images/user.png"
+        >
       </div>
-<!--      <span>{{$t('message')}}</span>-->
+    </div>
+    <div class="right-head">
+      <div class="language">
+        <span @click="setLang('en')">ru/eng</span>
+      </div>
+      <div class="portfolio-btn">
+        <a href="#">Portfolio</a>
+      </div>
+<!--      <div @click="menu_show = !menu_show" class="menu cursor-pointer">-->
+<!--        <div class="menu-icon">-->
+<!--          <span :class="['menu-line menu-line1', {'menu-show': menu_show}]"></span>-->
+<!--          <span :class="['menu-line menu-line2', {'menu-show': menu_show}]"></span>-->
+<!--          <span :class="['menu-line menu-line3', {'menu-show': menu_show}]"></span>-->
+<!--        </div>-->
+<!--        &lt;!&ndash;      <span>{{$t('message')}}</span>&ndash;&gt;-->
+<!--      </div>-->
+      <div class="contact-me-btn">
+        <button>Say Hello</button>
+      </div>
     </div>
   </div>
 </template>
@@ -69,12 +79,14 @@
   .header {
     position: fixed;
     width: 100%;
-    display: grid;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
     grid-template-columns: auto auto 1fr;
     align-items: center;
-    padding: 3px 80px;
+    padding: 20px 80px 5px;
     background: #ffffff;
-    border-bottom: 1px solid #ddd8;
+    /*border-bottom: 1px solid #ddd8;*/
     transition: .4s all;
     transform: translateY(0);
     opacity: 0.7;
@@ -96,14 +108,52 @@
     .user-photo{
       height: 60px;
       img{
+        border: 2px solid #8C43FF;
+        border-radius: 50%;
         opacity: 0;
         width: 60px;
+      }
+    }
+    .right-head {
+      width: 20%;
+      align-items: center;
+      justify-content: space-between;
+      display: flex;
+      flex-flow: row wrap;
+
+      .contact-me-btn button{
+        border: 2px solid #8C43FF;
+        font-weight: 400;
+        font-size: 18px;
+        padding: 7px 15px;
+        background-color: transparent;
+        border-radius: 290486px;
+        color: #8C43FF;
+      }
+      .contact-me-btn button:hover {
+        background-color: #8C43FF;
+        border-color: #8C43FF;
+        color: #fff;
+      }
+      .portfolio-btn a {
+        text-decoration: none;
+        font-size: 17px;
+        font-weight: 300;
+        color: #293347;
+      }
+      .portfolio-btn a:hover {
+        color: #8C43FF;
       }
     }
     .language {
 
       span{
-        font-family: 'Artifika', serif;
+        font-size: 17px;
+        font-weight: 300;
+        color: #293347;
+      }
+      span:hover {
+        color: #8C43FF;
       }
     }
     .menu{
