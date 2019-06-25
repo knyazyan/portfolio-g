0af951e584893eed5ff90adb86e4707b2f0475dc
armen1995 <arknyazyan@gmail.com>
2019-06-25 12:06:35 +0400

    fixed

--- src/views/home/Technologies.vue
+++ src/views/home/Technologies.vue
@@ -1,127 +1,132 @@
 <template>
   <div class="technologies">
     <div class="technologies-text">
-      <h1>здравствуйте я Гурген: Oчень рад вас видеть</h1>
-      <p>С начала моего путешествия в качестве внештатного дизайнера около 8 лет назад, я я сделал уборку на улице:</p>
-      <p>работал очень много работал день и ночь,пока мои друзья(роланд, мушег) подошли  ка мне и предлагали у них в офисесделать уборку</p>
-      <p>digital products for both business and consumer use. I'm quietly confident, naturally curious,</p>
-      <p> and perpetually working on improving my chops one design problem at a time.</p>
-    </div>
-
-    <div class="boxes">
-      <div class="box">
-        <div class="logo">
-          <img src="../../assets/images/about-me/react-512.png" width="100px">
-        </div>
-        <div class="title">
-          <h5>Designer</h5>
-        </div>
-        <div class="title-desc">
-          <p>
-            I value simple content structure,
-            clean design patterns, and
-            thoughtful interactions.
-          </p>
-        </div>
-        <div class="tools">
-          <div class="title">
-            <h5>Things I enjoy designing:</h5>
-          </div>
-          <div class="title-desc">
-            <p>
-              UX, UI, Web, Mobile, Apps, Logos
-            </p>
-          </div>
-        </div>
-
-        <div class="tittle1">
-          <h5>Design Tools:</h5>
-          <p>Balsamiq Mockups</p>
-          <p>Figma</p>
-          <p>Invision</p>
-          <p>Marvel</p>
-          <p>Sketch</p>
-          <p>Webflow</p>
-          <p>Zeplin</p>
-        </div>
-      </div>
-      <div class="box">
-        <div class="logo">
-          <img src="../../assets/images/about-me/1_yeAO-nwsAqnzr7k-zoDkoQ.png" width="100px">
-        </div>
-        <div class="title">
-          <h5>Front-end Developer</h5>
-        </div>
-        <div class="title-desc">
-          <p>
-            I like to code things from
-            scratch, and enjoy
-            bringing ideas to life in
-            the browser.
-          </p>
-        </div>
-        <div class="tools">
-          <div class="title">
-            <h5>Languages I speak:</h5>
-          </div>
-          <div class="title-desc">
-            <p>
-              HTML, Pug, Slim, CSS, Sass, Less
-            </p>
-          </div>
-        </div>
-
-        <div class="tittle1">
-          <h5> Dev Tools:</h5>
-          <p>Atom </p>
-          <p>Bitbucket</p>
-          <p>Bootstrap</p>
-          <p>Bulma</p>
-          <p>Codekit</p>
-          <p>Codepen</p>
-          <p>Github</p>
-          <p>Gitlab</p>
-          <p>Terminal</p>
-        </div>
-      </div>
-      <div class="box">
-        <div class="logo">
-          <img src="../../assets/images/about-me/загруженное.png" width="100px">
-        </div>
-        <div class="title">
-          <h5>Mentor</h5>
-        </div>
-        <div class="title-desc">
-          <p>
-            I genuinely care about
-            people, and love helping
-            fellow designers work on
-            their craft.
-          </p>
-        </div>
-        <div class="tools">
-          <div class="title">
-            <h5>Experiences I draw from:</h5>
-          </div>
-          <div class="title-desc">
-            <p>
-              UX/UI, Product design, Freelancing
-            </p>
-          </div>
-        </div>
-
-        <div class="tittle1">
-          <h5> Mentor Stats:</h5>
-          <p>4 years experience</p>
-          <p>26 short courses</p>
-          <p>34 bootcamps</p>
-          <p>100+ students</p>
-          <p>1,000+ mentor sessions</p>
-          <p>60+ group critiques</p>
-          <p>9,000+ comments</p>
-        </div>
+      <div>
+        <h1>здравствуйте я Гурген: Oчень рад вас видеть</h1>
+        <p>С начала моего путешествия в качестве внештатного дизайнера около 8 лет назад, я я сделал уборку на улице:</p>
+        <p>работал очень много работал день и ночь,пока мои друзья(роланд, мушег) подошли  ка мне и предлагали у них в офисесделать уборку</p>
+        <p>digital products for both business and consumer use. I'm quietly confident, naturally curious,</p>
+        <p> and perpetually working on improving my chops one design problem at a time.</p>
       </div>
     </div>
+
+    <div class="boxes-wrap">
+      <div class="boxes">
+        <div class="box">
+          <div class="logo">
+            <img src="../../assets/images/about-me/react-512.png" width="100px">
+          </div>
+          <div class="title">
+            <h5>Designer</h5>
+          </div>
+          <div class="title-desc">
+            <p>
+              I value simple content structure,
+              clean design patterns, and
+              thoughtful interactions.
+            </p>
+          </div>
+          <div class="tools">
+            <div class="title">
+              <h5>Things I enjoy designing:</h5>
+            </div>
+            <div class="title-desc">
+              <p>
+                UX, UI, Web, Mobile, Apps, Logos
+              </p>
+            </div>
+          </div>
+
+          <div class="tittle1">
+            <h5>Design Tools:</h5>
+            <p>Balsamiq Mockups</p>
+            <p>Figma</p>
+            <p>Invision</p>
+            <p>Marvel</p>
+            <p>Sketch</p>
+            <p>Webflow</p>
+            <p>Zeplin</p>
+          </div>
+        </div>
+        <div class="box">
+          <div class="logo">
+            <img src="../../assets/images/about-me/1_yeAO-nwsAqnzr7k-zoDkoQ.png" width="100px">
+          </div>
+          <div class="title">
+            <h5>Front-end Developer</h5>
+          </div>
+          <div class="title-desc">
+            <p>
+              I like to code things from
+              scratch, and enjoy
+              bringing ideas to life in
+              the browser.
+            </p>
+          </div>
+          <div class="tools">
+            <div class="title">
+              <h5>Languages I speak:</h5>
+            </div>
+            <div class="title-desc">
+              <p>
+                HTML, Pug, Slim, CSS, Sass, Less
+              </p>
+            </div>
+          </div>
+
+          <div class="tittle1">
+            <h5> Dev Tools:</h5>
+            <p>Atom </p>
+            <p>Bitbucket</p>
+            <p>Bootstrap</p>
+            <p>Bulma</p>
+            <p>Codekit</p>
+            <p>Codepen</p>
+            <p>Github</p>
+            <p>Gitlab</p>
+            <p>Terminal</p>
+          </div>
+        </div>
+        <div class="box">
+          <div class="logo">
+            <img src="../../assets/images/about-me/загруженное.png" width="100px">
+          </div>
+          <div class="title">
+            <h5>Mentor</h5>
+          </div>
+          <div class="title-desc">
+            <p>
+              I genuinely care about
+              people, and love helping
+              fellow designers work on
+              their craft.
+            </p>
+          </div>
+          <div class="tools">
+            <div class="title">
+              <h5>Experiences I draw from:</h5>
+            </div>
+            <div class="title-desc">
+              <p>
+                UX/UI, Product design, Freelancing
+              </p>
+            </div>
+          </div>
+
+          <div class="tittle1">
+            <h5> Mentor Stats:</h5>
+            <p>4 years experience</p>
+            <p>26 short courses</p>
+            <p>34 bootcamps</p>
+            <p>100+ students</p>
+            <p>1,000+ mentor sessions</p>
+            <p>60+ group critiques</p>
+            <p>9,000+ comments</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 <script>
@@ -150,6 +155,7 @@
     background-color: #269AFF;
     color: #ffffff;
     font-family: sans-serif;
+
     h1{
       text-align: center;
       padding-top: 120px;
@@ -161,9 +167,11 @@
       font-size: 20px;
     }
   }
-
+  .boxes-wrap{
+    padding:0 80px;
+  }
   .boxes{
-    width: 60%;
+    width:100%;
     border: 1px solid #b1b1b1;
     border-radius: 30px;
     background-color: #ffffff;
